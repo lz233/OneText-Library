6b785ddb0c277c1d9e43a83f6928b2769fd6a1b0
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2022-01-15 22:17:11 +0800

    Api demo (#38)
    api demo

---
+++ api/index.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type OnetextData struct {
+	Text string   `json:"text"`
+	By   string   `json:"by,omitempty"`
+	From string   `json:"from,omitempty"`
+	Time []string `json:"time"`
+	Uri  string   `json:"uri,omitempty"`
+}
+
+var JsonData []OnetextData
+
+func init() {
+	resp, err := http.Get("https://raw.githubusercontent.com/lz233/OneText-Library/master/OneText-Library.json")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	jsonByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	_ = json.Unmarshal(jsonByte, &JsonData)
+}
+
+func HandleOnetext(w http.ResponseWriter, r *http.Request) {
+	n := rand.New(rand.NewSource(time.Now().UnixNano()))
+	bytes, _ := json.MarshalIndent(JsonData[n.Intn(len(JsonData))], "", "    ")
+	_, _ = fmt.Fprint(w, string(bytes))
+}
