d52498c5239b63b798ff37345083dd1885840a73
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2022-01-21 16:37:17 +0800

    Move the core code to my own repo (#40)
    * update api
    
    * update api
    
    * update go.mod

--- api/index.go
+++ api/index.go
@@ -1,40 +1,19 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"math/rand"
+	onetext "github.com/XiaoMengXinX/OneTextAPI-Go"
 	"net/http"
-	"time"
 )
 
-type OnetextData struct {
-	Text string   `json:"text"`
-	By   string   `json:"by,omitempty"`
-	From string   `json:"from,omitempty"`
-	Time []string `json:"time"`
-	Uri  string   `json:"uri,omitempty"`
-}
-
-var JsonData []OnetextData
+var o onetext.OneText
 
 func init() {
-	resp, err := http.Get("https://raw.githubusercontent.com/lz233/OneText-Library/master/OneText-Library.json")
+	err := o.GetUrl("https://raw.githubusercontent.com/lz233/OneText-Library/master/OneText-Library.json")
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
-	jsonByte, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	_ = json.Unmarshal(jsonByte, &JsonData)
 }
 
 func HandleOnetext(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json; charset=utf-8")
-	n := rand.New(rand.NewSource(time.Now().UnixNano()))
-	bytes, _ := json.MarshalIndent(JsonData[n.Intn(len(JsonData))], "", "    ")
-	_, _ = fmt.Fprint(w, string(bytes))
+	o.Handler(w, r)
 }
